d234aa85ab72b15d1ed7f2565c735b51820ec1df
SafernAchieve <126269703+SafernAchieve@users.noreply.github.com>
2024-06-20 16:51:30 -0400

    Vue with vuetify working Added to an exsisting project

--- cc/src/main.js
+++ cc/src/main.js
@@ -6,24 +6,12 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-// Router
-import router from './router'
-
 // Components
 import App from './App.vue'
 
-// Create Vuetify instance
 const vuetify = createVuetify({
   components,
   directives,
 })
 
-// Create Vue app instance
-const app = createApp(App)
-
-// Use Vuetify and Router
-app.use(vuetify)
-app.use(router)
-
-// Mount the app
-app.mount('#app')
+createApp(App).use(vuetify).mount('#app')
